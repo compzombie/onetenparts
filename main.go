aeb67e342bf1836604f976bc7a26849b1fd82359
compzombie <mlmyerson@gmail.com>
2022-01-07 07:44:38 -0500

    Refactoring input misinterpretation

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func flagOutCheck(flgout *string) {
 	//check submitted flags
 	if *flgout != "b" && *flgout != "s" && *flgout != "d" && *flgout != "a" {
 		err := InputError{"-out flag input malformed."}
+		fmt.Println(err)
 		panic(err)
 	}
 }
@@ -31,16 +32,18 @@ func flagLatCheck(flgout *string) {
 	//check submitted flags
 	if *flgout != "b" && *flgout != "s" && *flgout != "d" && *flgout != "a" {
 		err := InputError{"-out flag input malformed."}
+		fmt.Println(err)
 		panic(err)
 	}
 }
 
 //an ic can only consist of <0>'s and <1>'s
-func icCheck(ic string) {
+func flagIcCheck(ic string) {
 	for i := 0; i < len(ic); i++ {
 		cell := ic[i]
 		if cell != '0' && cell != '1' {
 			err := InputError{"-out flag input malformed."}
+			fmt.Println(err)
 			panic(err)
 		}
 	}
@@ -50,6 +53,7 @@ func icCheck(ic string) {
 func flagGenCheck(gens *int) {
 	if *gens <= 0 {
 		err := InputError{"gen must be greater than 0 and a valid integer"}
+		fmt.Println(err)
 		panic(err)
 	}
 }
@@ -63,6 +67,47 @@ func InitLattice(ic string) *[]string {
 	return &lattice
 }
 
+//toroidal
+func ParseRowToroid(row string) string {
+	out := ""
+	for i := 0; i < len(row); i++ {
+		leftdex, middex, ritdex := -1, -1, -1
+
+		if (i - 1) < 0 {
+			leftdex = len(row) - 1
+		} else {
+			leftdex = i - 1
+		}
+
+		middex = i
+
+		if (i + 1) >= len(row) {
+			ritdex = 0
+		} else {
+			ritdex = i + 1
+		}
+
+		//state of each neighbor
+		lef, lerr := strconv.Atoi(string(row[leftdex]))
+		mid, merr := strconv.Atoi(string(row[middex]))
+		rit, rerr := strconv.Atoi(string(row[ritdex]))
+		if lerr != nil {
+			panic(lerr)
+		}
+		if merr != nil {
+			panic(merr)
+		}
+		if rerr != nil {
+			panic(rerr)
+		}
+
+		state := 4*lef + 2*mid + 1*rit
+
+		out += strconv.Itoa(state)
+	}
+	return out
+}
+
 func main() {
 	//graph output - binary/state/domain/all
 	flagOut := flag.String("out", "b", "sets output parameters -(b)inary -(s)tate -(d)omain -(a)ll")
@@ -71,7 +116,10 @@ func main() {
 	flagLat := flag.String("lat", "t", "sets lattice properties -(t)oroidal -(z)ero-edged -(o)ne-edged -(u)nbounded -(a)ll")
 
 	//number of generations to compute
-	flagGen := flag.Int("gens", 10, "sets number of generations to compute before stopping")
+	flagGens := flag.Int("gens", 10, "sets number of generations to compute before stopping")
+
+	//initial condition of 1's and 0's
+	flagIc := flag.String("ic", "00000100000", "initial condition of ca")
 	flag.Parse()
 
 	if len(flag.Args()) <= 0 {
@@ -85,17 +133,21 @@ func main() {
 
 	flagOutCheck(flagOut)
 	flagLatCheck(flagLat)
-	flagGenCheck(flagGen)
-	icCheck(ic)
+	flagGenCheck(flagGens)
+	flagIcCheck(*flagIc)
 
 	//generate state ca and work off of that
 	lattice := InitLattice(ic)
+	for i := 1; i < *flagGens; i++ {
+		*lattice = append(*lattice, ParseRowToroid((*lattice)[0]))
+	}
+
 	//output
 
 	fmt.Println("Program Arguments")
 	fmt.Println("-out: " + *flagOut)
 	fmt.Println("-lat: " + *flagLat)
-	fmt.Println("-gens" + strconv.Itoa(*flagGen))
+	fmt.Println("-gens" + strconv.Itoa(*flagGens))
 	fmt.Println("IC: " + flag.Args()[0])
 	fmt.Println(time.Now())
 	fmt.Println(lattice)
