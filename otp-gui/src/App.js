e431020f2669038fc398189f7fed1d8c743044d1
compzombie <mlmyerson@gmail.com>
2022-01-09 13:19:08 -0500

    OptionsDrawer started

--- otp-gui/src/App.js
+++ otp-gui/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Grid from './components/Grid.js'
+import OptionsDrawer from './components/OptionsDrawer';
 
 function App() {
   return (
     <div className="App">
+      <OptionsDrawer />
       <Grid />
     </div>
   );
